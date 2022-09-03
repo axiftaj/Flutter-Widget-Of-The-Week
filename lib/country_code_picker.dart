0207b03764aa20124e181573e691bbb0f911fe31
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-09-03 20:03:33 +0500

    country code picker widget added

---
+++ lib/country_code_picker.dart
@@ -0,0 +1,56 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+
+
+class CountryCodePickerWidget extends StatefulWidget {
+  const CountryCodePickerWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CountryCodePickerWidget> createState() => _CountryCodePickerWidgetState();
+}
+
+class _CountryCodePickerWidgetState extends State<CountryCodePickerWidget> {
+  String countryCode = '' ;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Country code picker'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children:  [
+          Text(countryCode.toString()),
+          Center(
+            child: TextButton(onPressed: (){
+              showCountryPicker(
+                  context: context,
+                  countryListTheme: CountryListThemeData(
+                    textStyle: TextStyle(fontSize: 20),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10)
+                    ),
+                    inputDecoration: InputDecoration(
+                      hintText: 'Start typing to sear' ,
+                      labelText: 'Search ' ,
+                    )
+                  ),
+                  favorite: ['PK'],
+                  onSelect: (Country value){
+                    print(value.countryCode.toString());
+                    print(value.phoneCode.toString());
+
+                    countryCode = value.phoneCode.toString();
+                    setState(() {
+
+                    });
+                  }
+              );
+            }, child: Text('Tap')),
+          )
+        ],
+      ),
+    );
+  }
+}
