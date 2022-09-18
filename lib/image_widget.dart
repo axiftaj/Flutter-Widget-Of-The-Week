b402e7bf81169d1dad604b6e03d0e3a1b0a7aa54
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-09-18 15:58:46 +0500

    image widget added

---
+++ lib/image_widget.dart
@@ -0,0 +1,74 @@
+
+
+import 'package:flutter/material.dart';
+
+class ImageWidget extends StatelessWidget {
+  const ImageWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ImageBuilderWidget(image: 'https://images.pexels.com/photos/13585848/pexels-photo-13585848.jpeg?cs=srgb&dl=pexels-bruno-silva-13585848.jpg&fm=jpg',),
+      ),
+    );
+  }
+}
+
+
+class ImageBuilderWidget extends StatelessWidget {
+
+  final String image ;
+  final double height, width ;
+  const ImageBuilderWidget({Key? key,required this.image ,  this.height = 200 , this.width = 200 }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+            )
+        ),
+        child: ClipRRect(
+          child: Image(
+            fit: BoxFit.cover,
+            height: 40,
+            width: 40,
+            image:  NetworkImage(image),
+            errorBuilder: (BuildContext context, Object exception,
+                StackTrace? stackTrace) {
+              return Container(
+                  height: height,
+                  width: width,
+                  child: const Icon(
+                    Icons.error,
+                    color: Colors.red,
+                  ));
+            },
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) return child ;
+              return SizedBox(
+                height: height,
+                width: width,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                        : null,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'animated_container_widget.dart';
 import 'country_code_picker.dart';
+import 'image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,18 +17,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedContainerWidget(),
+      home: ImageWidget(),
     );
   }
 }
