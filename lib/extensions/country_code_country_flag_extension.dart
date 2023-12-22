e6ce352de64520b868a4f49afe85f21500cb731e
Asif Taj <axiftaj@gmail.com>
2023-12-22 11:45:22 +0500

    convert country code to country flag extension added

--- lib/extensions/country_code_country_flag_extension.dart
+++ lib/extensions/country_code_country_flag_extension.dart
@@ -18,6 +18,7 @@ class CountryCodeToFlagExtension extends StatelessWidget {
 
 extension ConvertFlag on String {
   String get toFlag {
+
     return  (this).toUpperCase().replaceAllMapped(RegExp(r'[A-Z]'),
             (match) => String.fromCharCode(match.group(0)!.codeUnitAt(0) + 127397));
   }
